76a652345ec9ccac8c6dc5e388b69e297fbd91ca
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-12-05 19:33:47 +0200

    Add files via upload

---
+++ HW060219/Citizen.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW060219
+{
+    class Citizen
+    {
+        public string _name;
+        public Citizen[] _children = new Citizen[0]; //prevent exeption
+        public readonly int _fatherID;
+        public readonly int _id;
+        public static int _numberOfCurrentCitizens = 0;
+        public const int _maximumNumberOfCitizens = 50_000;
+        public static int _configuration = 1; //must be static in order to count the same way
+        static Random rnd = new Random(); // happens only once
+        static int[] LookUpids = new int[_maximumNumberOfCitizens];
+
+        public Citizen(string name, int fatherID)
+        {
+            bool IsIdExist = true;
+            int newID = rnd.Next(1, _maximumNumberOfCitizens + 1);
+            _name = name;
+            _fatherID = fatherID;
+            _numberOfCurrentCitizens++;
+            if (_configuration == 1)
+                _id = _numberOfCurrentCitizens;
+            else if (_configuration == 2)
+            {
+                while (IsIdExist)
+                {
+                    for (int i = 0; i < LookUpids.Length; i++)
+                    {
+                        if (LookUpids[i] == newID)
+                        {
+                            IsIdExist = true;
+                            break;
+                        }
+                        else
+                            IsIdExist = false;
+                    }
+                    newID = rnd.Next(1, _maximumNumberOfCitizens + 1);
+                }
+                _id = newID;
+                LookUpids[_numberOfCurrentCitizens] = _id;
+            }
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine($"number Of Current Citizens: {_numberOfCurrentCitizens}");
+        }
+        public static bool ReachHalfOfMaximumSize()
+        {
+            if (_numberOfCurrentCitizens >= _maximumNumberOfCitizens)
+                return true;
+            else
+                return false;
+        }
+        public void PrintID()
+        {
+            Console.WriteLine($"The id is: {_id}");
+        }
+        public void PrintGapBtweenMyIDandFather()
+        {
+            Console.WriteLine($"The Gap between my ID and my father is: {_id} - {_fatherID} = {_id - _fatherID}");
+        }
+        //Doesn't have to be in the ctor:
+        public void SetChildren(Citizen[] AllCitizens)
+        {
+            _children = AllCitizens;
+        }
+
+        public override string ToString()
+        {
+            return $"Citizen {_name} id {_id} father id: {_fatherID}";
+        }
+
+    }
+}
---
+++ HW060219/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW060219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen Shmuel = new Citizen("Shmuel", 4771);
+            //Children:
+            Citizen c1 = new Citizen("David", 8221);
+            Citizen c2 = new Citizen("Haim", 3664);
+            Citizen c3 = new Citizen("Oren", 6552);
+
+            Citizen[] AllChilds =
+            {
+                c1,
+                c2,
+                c3
+            };
+            Shmuel.SetChildren(AllChilds);
+
+
+        }
+
+        static bool HasChildren(Citizen Anonym)
+        {
+            if (Anonym._children.Length > 0)
+                return true;
+            else
+                return false;
+        }
+
+        static bool CheckValidity(Citizen Anonym)
+        {
+            //bool Check = false;
+            //if (Anonym._children.Length > 0)
+            //{
+            //    for (int i = 0; i < Anonym._children.Length; i++)
+            //    {
+            //        if (Anonym._id == Anonym._children[i]._fatherID)
+            //            Check = true;
+            //        else
+            //        {
+            //            Check = false;
+            //            break;
+            //        }
+            //    }
+            //}
+            //return Check;
+            
+            //also good:
+            foreach (Citizen kid in Anonym._children)
+            {
+                if (kid._fatherID != Anonym._id)
+                    return false;
+            }
+            return true;
+            
+        }
+    }
+}
